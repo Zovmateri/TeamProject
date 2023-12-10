8634f1f184973bcad7d2cf793cb8a940b8fcc3b9
Nassif <basmanassif9@gmail.com>
2023-12-10 15:17:13 +0300

    oleg suffering

--- KitchenAmateur/metro.config.js
+++ KitchenAmateur/metro.config.js
@@ -3,3 +3,17 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 config.resolver.assetExts.push('sqlite')
 module.exports = config;
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-css-transformer")
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "css"]
+    }
+  };
+})();
